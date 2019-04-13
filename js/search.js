c12f00daab23fcf4d5665d18a05a3e75b750d835
Gemmy Orvayne <joyananda.gi@gmail.com>
2019-04-13 19:03:16 -0400

    search results done
    bitch

--- js/search.js
+++ js/search.js
@@ -31,7 +31,7 @@
   }
 
   var searchTerm = getQueryVariable('query');
-
+    document.getElementById('search-term').textContent=searchTerm;
   if (searchTerm) {
     // Initalize lunr with the fields it will be searching on. I've given title
     // a boost of 10 to indicate matches on this field are more important.
